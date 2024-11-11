6ad32861fbab910bf275b9e550371a0f4c7cb81a
Maekiz <manoa039@osloskolen.no>
2024-11-11 10:26:02 +0100

    changes

--- golobal.gd
+++
@@ -1,1 +0,0 @@
-var cheese = 0
--- script/player.gd
+++ script/player.gd
@@ -1,9 +1,25 @@
-extends Node
+extends CharacterBody2D
 
+var direction : Vector2 = Vector2()
+@export var playerSpeed = 200
+@export var gravity = 30
+@export var jumpForce = 300
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y += gravity
+		if Input.is_action_just_pressed("up"):
+			velocity.y = -jumpForce
+	
+
+	
+	var horizontal_direction = Input.get_axis("left", "right")
+	velocity.x = playerSpeed*horizontal_direction
+	move_and_slide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
